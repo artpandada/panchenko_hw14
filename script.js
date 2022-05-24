ff3df924d86d126adf61c7976881856d39cf3cb7
Daria Panchenko <user576717@gmail.com>
2022-05-24 11:07:32 +0100

    edited my error

--- script.js
+++ script.js
@@ -4,21 +4,8 @@ function Student(name, surname, year) {
     this.name = name;
     this.surname = surname;
     this.yearBorn = year;
-    this.visit = [];
-    this.grade = [];
-    this.average = function (value) {
-
-        if (value.length === 0) {
-            return 0;
-        }
-
-        let sum = 0;
-        for (let i = 0; i < value.length; i++) {
-            sum += value[i];
-        }
-
-        return sum / value.length;
-    }
+    this.visit = new Array(10);
+    this.grade = new Array(10);
 }
 
 
@@ -29,34 +16,73 @@ Student.prototype.age = function () {
 
 Student.prototype.present = function () {
 
-    if (this.visit.length < 10) {
-        this.visit[this.visit.length] = true;
+    for (let i = 0; i < this.visit.length; i++) {
+
+        if (this.visit[i] === undefined) {
+            this.visit[i] = true;
+            break;
+        }
+
     }
+
+
 }
 
 Student.prototype.absent = function () {
 
-    if (this.visit.length < 10) {
+    for (let i = 0; i < this.visit.length; i++) {
 
-        this.visit[this.visit.length] = false;
+        if (this.visit[i] === undefined) {
+            this.visit[i] = false;
+            break;
+        }
+
     }
 }
 
 Student.prototype.mark = function (grade) {
-    if (typeof grade === 'number' && grade >= 0 && grade <= 10 && this.grade.length < 10) {
-        this.grade[this.grade.length] = grade;
+
+    if (typeof grade === 'number' && grade >= 0 && grade <= 10) {
+
+        for (let i = 0; i < this.grade.length; i++) {
+
+            if (this.grade[i] === undefined) {
+                this.grade[i] = grade;
+                break;
+
+            }
+        }
+    }
+}
+
+Student.prototype.average = function (value) {
+    let length = 0;
+    let sum = 0;
+
+    for (let i = 0; i < value.length; i++) {
+
+        if (value[i] !== undefined) {
+            sum += value[i];
+            length++;
+        }
     }
 
+    if (length === 0) {
+        return 0;
+    }
+
+    return sum / length;
 }
+
+
 Student.prototype.summary = function () {
     if (this.average(this.grade) > 9 && this.average(this.visit) > 0.9) {
         return "Ути какой молодчинка!";
     } else if (this.average(this.grade) > 9 || this.average(this.visit) > 0.9) {
-        return "Норм, но можно лучше";
+        return "Норм,  но можно лучше";
     } else {
         return "Редиска!";
     }
-
 }
 
 
@@ -69,20 +95,33 @@ let user = {
     yearBorn: 2010
 }
 let st = new Student(user.name, user.surname, user.yearBorn);
+console.log(st.visit[0])
+st.present();
+st.present();
+st.present();
+st.present();
 
 st.present();
-st.absent();
 st.present();
 st.present();
+st.present();
+
 
 st.mark(10);
 st.mark(10);
 st.mark(10);
 st.mark(-1);
 st.mark(10);
+
+st.mark('n');
 st.mark(10);
-st.mark(9);
-st.mark(9);
+st.mark(10);
+st.mark(10);
+st.mark(-1);
+st.mark(10);
+st.mark(10);
+
+
 st.mark('n');
 
 console.log(st.age());
